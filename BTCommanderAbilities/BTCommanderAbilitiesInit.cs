c596014bc63ae03598d46bdfa027008e52ef6f98
Marc Bohler <mgrbo@web.de>
2020-04-28 15:59:11 +0200

    Add project files.

---
+++ BTCommanderAbilities/BTCommanderAbilitiesInit.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace BTCommanderAbilities
+{
+    class BTCommanderAbilitiesInit
+    {
+        public static void Init(string dir, string sett)
+        {
+            var harmony = HarmonyInstance.Create("com.github.mcb5637.BTCommanderAbilities");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ BTCommanderAbilities/SimGameState_CanPilotTakeAbility.cs
@@ -0,0 +1,21 @@
+﻿using BattleTech;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BTCommanderAbilities
+{
+    [HarmonyPatch(typeof(SimGameState), "CanPilotTakeAbility")]
+    class SimGameState_CanPilotTakeAbility
+    {
+        public static void Postfix(PilotDef p, ref bool __result)
+        {
+            if (p.PilotTags != null && p.PilotTags.Contains("commander_player"))
+            {
+                __result = true;
+            }
+        }
+    }
+}
